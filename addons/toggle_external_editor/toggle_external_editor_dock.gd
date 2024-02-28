35ca588b420436f6428f4b978a984c77ee733810
Cuppar <cuppar@163.com>
2024-02-28 19:14:09 +0800

    ss

--- addons/toggle_external_editor/toggle_external_editor_dock.gd
+++ addons/toggle_external_editor/toggle_external_editor_dock.gd
@@ -1,20 +1,15 @@
 @tool
 extends Control
 
-const USE_EXTERNAL_EDITOR_SETTING_STRING:String = "text_editor/external/use_external_editor"
+const USE_EXTERNAL_EDITOR_SETTING_STRING: String = "text_editor/external/use_external_editor"
 
 @onready var check_box: CheckBox = $HBoxContainer/CheckBox
-
 
 func _on_link_button_pressed() -> void:
 	check_box.button_pressed = not check_box.button_pressed
 
-
 func _on_check_box_toggled(toggled_on: bool) -> void:
-	############################
-	## Error here:
 	var settings = EditorInterface.get_editor_settings()
-	#############################
 	var is_enable_external_editor = settings.get(USE_EXTERNAL_EDITOR_SETTING_STRING)
 	
 	if toggled_on:
@@ -23,4 +18,3 @@ func _on_check_box_toggled(toggled_on: bool) -> void:
 	else:
 		print('Plugin<toggle_external_editor>: disable external editor')
 		settings.set(USE_EXTERNAL_EDITOR_SETTING_STRING, false)
-
